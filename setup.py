7cc661e011aae7924db894733ec7a3cc6bddebdd
Antoine Bertin <diaoulael@gmail.com>
2012-02-27 13:01:33 +0100

    Fix license header in setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@
 # This file is part of pyextdirect.
 #
 # pyextdirect is free software; you can redistribute it and/or modify it under
-# the terms of the Lesser GNU Lesser General Public License as published by
+# the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # pyextdirect is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# Lesser GNU Lesser General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the Lesser GNU Lesser General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with pyextdirect.  If not, see <http://www.gnu.org/licenses/>.
 from setuptools import setup
 import pyextdirect.infos
